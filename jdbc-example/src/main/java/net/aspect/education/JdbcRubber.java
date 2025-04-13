8256dcc091dafc2f93dd182baf68c4f3187947d1
Aspect-your-love <kras.developer@yandex.ru>
2025-04-13 18:30:09 +0700

    JDBC

--- jdbc-example/src/main/java/net/aspect/education/JdbcRubber.java
+++ jdbc-example/src/main/java/net/aspect/education/JdbcRubber.java
@@ -2,14 +2,15 @@ package net.aspect.education;
 
 import net.aspect.education.utils.ConnectionManager;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class JdbcRubber {
 
     public static void main(String[] args) throws SQLException {
-        try (Connection connection = ConnectionManager.open()) {
-            System.out.println(connection.getTransactionIsolation());
+        try(Connection open = ConnectionManager.open()){
+            DatabaseMetaData metaData = open.getMetaData();
+            ResultSet catalogs = metaData.getCatalogs();
+            while(catalogs.next()) System.out.println(catalogs.getString(1));
         }
     }
 }
