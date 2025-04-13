feb8b513ccf92955976d36be98eda9d96c62f73e
Aspect-your-love <kras.developer@yandex.ru>
2025-04-14 01:36:47 +0700

    JDBC. Transactional

--- jdbc-example/src/main/java/net/aspect/education/JdbcRubber.java
+++ jdbc-example/src/main/java/net/aspect/education/JdbcRubber.java
@@ -7,10 +7,17 @@ import java.sql.*;
 public class JdbcRubber {
 
     public static void main(String[] args) throws SQLException {
-        try(Connection open = ConnectionManager.open()){
+        try (Connection open = ConnectionManager.get()) {
+            /*
             DatabaseMetaData metaData = open.getMetaData();
             ResultSet catalogs = metaData.getCatalogs();
-            while(catalogs.next()) System.out.println(catalogs.getString(1));
+            while (catalogs.next()) System.out.println(catalogs.getString(1));*/
+
+            String sql = "SELECT * FROM employee";
+            Statement statement = open.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            System.out.println(sql);
+            while (resultSet.next()) System.out.println(resultSet.getString("first_name"));
         }
     }
 }
