5bc8a21a323eac83a2449f46bf598e8c7a195dce
Aspect-your-love <kras.developer@yandex.ru>
2025-04-26 13:50:45 +0700

    Gradle. Work with project.

--- gradle-start/src/main/java/net/aspect/education/Main.java
+++ gradle-start/src/main/java/net/aspect/education/Main.java
@@ -3,5 +3,8 @@ package net.aspect.education;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        var str = new String("Hello");
+
+        System.out.println(str);
     }
 }
---
+++ gradle-start/src/test/java/net/aspect/education/Main.java
@@ -0,0 +1,12 @@
+package net.aspect.education;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Main {
+
+    @Test
+    public void tesInstance(){
+        Assertions.assertTrue(true);
+    }
+}
